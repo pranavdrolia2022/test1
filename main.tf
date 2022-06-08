8a6f8f34ca36f02813cb66c4298b54ac794582b3
HiteshGupta41420 <guptahitesh41420@gmail.com>
2022-06-08 22:18:42 +0530

    updated main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform{
+    backend "s3" {
+        bucket = "terraform-hitesh-bucket3"
+        key = "state_file/terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "terraform_state"
+    }
+}
+
 resource "aws_sns_topic" "sns1" {
   name = "test-sns-topic1"
 }
